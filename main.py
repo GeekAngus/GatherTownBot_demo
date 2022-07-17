1f0950f2bd46e51a4f9445f57621cbe6a029fa38
Angus <googolangus@gmail.com>
2022-07-17 23:38:12 +0800

    handle empty options for end of question list

--- main.py
+++ main.py
@@ -212,7 +212,8 @@ async def on_raw_reaction_add(payload):
                 embed_field_name = ra_msg_dict[chan][k]['msg_q']
                 embed_field_value = "See you soon.."
                 if ra_msg_dict[chan][k]['msg_t'] == 'q_select' :
-                    embed_field_value = ""
+                    if len(ra_msg_dict[chan][k]['options']) > 0:
+                        embed_field_value = ""
                     for opt in ra_msg_dict[chan][k]['options']:
                         embed_field_value += [*num_ra_list][int(opt) - 1]
                         embed_field_value += ra_msg_dict[chan][k]['options'][opt]
